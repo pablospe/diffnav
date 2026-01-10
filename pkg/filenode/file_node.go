8f495df995080e522edf160a2ddd6e1b9c63fa07
Pablo Speciale <pablospe@users.noreply.github.com>
2026-01-10 10:16:41 +0100

    feat: add configurable icon style for file tree
    Add `icons` config option with three choices:
    - "nerd-fonts": uses Nerd Font glyphs
    - "unicode": uses unicode symbols (+, ⛌, ●)
    - "ascii": uses plain ascii (+, x, *) [default]
    
    Press 'i' to cycle through icon styles.

--- pkg/filenode/file_node.go
+++ pkg/filenode/file_node.go
@@ -2,14 +2,10 @@ package filenode
 
 import (
 	"path/filepath"
-	"strings"
 
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/tree"
-
-	"github.com/dlvhdr/diffnav/pkg/constants"
-	"github.com/dlvhdr/diffnav/pkg/utils"
 )
 
 // Icon style constants.
@@ -31,53 +27,34 @@ func (f FileNode) Path() string {
 }
 
 func (f FileNode) Value() string {
-	icon := " "
-	status := " " + f.getStatusIcon()
-
-	depthWidth := f.Depth * 2
-	iconsWidth := lipgloss.Width(icon) + lipgloss.Width(status)
-	nameMaxWidth := constants.OpenFileTreeWidth - depthWidth - iconsWidth
-	base := filepath.Base(f.Path())
-	name := utils.TruncateString(base, nameMaxWidth)
-
-	spacerWidth := constants.OpenFileTreeWidth - lipgloss.Width(name) - iconsWidth - depthWidth
-	if len(name) < len(base) {
-		spacerWidth = spacerWidth - 1
-	}
-	spacer := ""
-	if spacerWidth > 0 {
-		spacer = strings.Repeat(" ", spacerWidth)
-	}
-
-	return lipgloss.JoinHorizontal(lipgloss.Top, icon, name, spacer, status)
+	icon := f.getIcon()
+	name := filepath.Base(f.Path())
+	return icon + " " + name
 }
 
-func (f FileNode) getStatusIcon() string {
-	color := f.StatusColor()
-	style := lipgloss.NewStyle().Foreground(color)
-
+func (f FileNode) getIcon() string {
 	switch f.IconStyle {
 	case IconsNerdFonts:
 		if f.File.IsNew {
-			return style.Render("")
+			return ""
 		} else if f.File.IsDelete {
-			return style.Render("")
+			return ""
 		}
-		return style.Render("")
+		return ""
 	case IconsUnicode:
 		if f.File.IsNew {
-			return style.Render("+")
+			return "+"
 		} else if f.File.IsDelete {
-			return style.Render("⛌")
+			return "⛌"
 		}
-		return style.Render("●")
-	default: // ascii
+		return "●"
+	default: // ascii (default)
 		if f.File.IsNew {
-			return style.Render("+")
+			return "+"
 		} else if f.File.IsDelete {
-			return style.Render("x")
+			return "x"
 		}
-		return style.Render("*")
+		return "*"
 	}
 }
 
@@ -88,7 +65,7 @@ func (f FileNode) StatusColor() lipgloss.Color {
 	} else if f.File.IsDelete {
 		return lipgloss.Color("1") // red
 	}
-	return lipgloss.Color("3") // yellow
+	return lipgloss.Color("3") // yellow/orange
 }
 
 func (f FileNode) String() string {
